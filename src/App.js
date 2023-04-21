ee51d7a693d0854422a721ecda4fd068bed77a34
Shaheer-Ahmd <shaheerahmad57575@gmail.com>
2023-04-21 22:11:41 +0500

    Changed some stylings (border radii)

--- src/App.js
+++ src/App.js
@@ -1,59 +1,65 @@
-import React from 'react'
-import './style.css'
-import Intro from './Intro'
-import Question from './Question'
-import {nanoid} from 'nanoid'
-
-
+import React from 'react';
+import './style.css';
+import Intro from './Intro';
+import Question from './Question';
+import { nanoid } from 'nanoid';
+// import Confetti from 'confetti'; Will add Confetti If the score is greater than 3  
 export default function App() {
-    
-    const [playing, setPlaying] = React.useState(false)
-    const [apiquestions, setApiquestions] = React.useState([])
-    const [submitted, setSubmitted] = React.useState(false)
-    const [selections, setSelections] = React.useState({})
-    const [score, setScore] = React.useState(0)
+  const [playing, setPlaying] = React.useState(false);
+  const [apiquestions, setApiquestions] = React.useState([]);
+  const [submitted, setSubmitted] = React.useState(false);
+  const [selections, setSelections] = React.useState({});
+  const [score, setScore] = React.useState(0);
 
-    React.useEffect(()=>{
-        if(!submitted) {
-        fetch("https://opentdb.com/api.php?amount=5&category=15&difficulty=medium&type=multiple")
-        .then(res=>res.json())
-        .then(data=>setApiquestions(data.results))}
-       
-    },[submitted])
-    
-    
-    const questions = apiquestions.map((apiquestion,index)=>{
-        const id = nanoid()
+  React.useEffect(() => {
+    if (!submitted) {
+      fetch(
+        'https://opentdb.com/api.php?amount=5&category=15&difficulty=medium&type=multiple'
+      )
+        .then((res) => res.json())
+        .then((data) => setApiquestions(data.results));
+    }
+  }, [submitted]);
+
+  const questions = apiquestions.map((apiquestion, index) => {
+    const id = nanoid();
     return (
-    <Question 
-    selections = {selections}
-    key={id}
-    id = {index}
-    setSelections = {setSelections}
-    
-    correct_answer={apiquestion.correct_answer}
-    incorrect_answers={apiquestion.incorrect_answers}
-    question={apiquestion.question}
-    submitted = {submitted}
-    score = {score}
-    setScore ={setScore}/>)}
-    )
-    return (
-        <div>
-            {playing ? 
-            submitted ? ({score}>3 ? <Confetti />: null) :
-            <div className = "main">
+      <Question
+        selections={selections}
+        key={id}
+        id={index}
+        setSelections={setSelections}
+        correct_answer={apiquestion.correct_answer}
+        incorrect_answers={apiquestion.incorrect_answers}
+        question={apiquestion.question}
+        submitted={submitted}
+        score={score}
+        setScore={setScore}
+        playing = {playing}
+      />
+    );
+  });
+
+  return (
+    <div>
+      {playing ? 
+        (
+          <div className='main'>
             {questions}
-            <div>
-            <button className ="Checkanswers" onClick ={()=>setSubmitted((prevsubmit)=>!prevsubmit)}>
-                {submitted ? "New Session":"Check Answers"}
-            </button>
-            <h2>
-                Score: {score}
-            </h2>
+            <div className = "footer">
+              <button
+                className='Checkanswers'
+                onClick={() => setSubmitted((prevsubmit) => !prevsubmit)}
+              >
+                {submitted ? 'New Session' : 'Check Answers'}
+              </button>
+              <h2>Score: {score}</h2>
             </div>
-            </div>
-             : <Intro playing={playing} setPlaying={setPlaying}/>}
-        </div>
-    )
+          </div>
+        )
+       : (
+        <Intro playing={playing} setPlaying={setPlaying} />
+      )}
+    </div>
+  );
 }
--- src/Question.js
+++ src/Question.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import "./style.css"
-import Option from './Option'
+import React from 'react';
+import "./style.css";
+import Option from './Option';
 
-export default function Question({id,score,setScore,selections,setSelections,submitted,correct_answer,incorrect_answers,question}) {
+export default function Question({id, playing, score, setScore, selections, setSelections, submitted, correct_answer, incorrect_answers, question}) {
    
-    const options = [...incorrect_answers ,correct_answer].map(option=> <Option id ={id}
-         option = {option} correct_answer ={correct_answer} 
-        score ={score} 
-        selections = {selections} setSelections ={setSelections}
-        submitted={submitted} setScore = {setScore}/>)
-    
+
+    let options = [...incorrect_answers , correct_answer].map(option => (
+        <Option
+            id={id}
+            option={option}
+            correct_answer={correct_answer}
+            score={score}
+            selections={selections}
+            setSelections={setSelections}
+            submitted={submitted}
+            setScore={setScore}
+        />
+    ));
+
     return (
-    <div className = "question">
-        <h2 > 
-        {question}
-        </h2>
-        <div className ="options">
-        {options}
+        <div className="question">
+            <h2>{question}</h2>
+            <div className="options">{options}</div>
+            <hr />
         </div>
-        <hr />
-        
-    </div>
-  )
+    );
 }
